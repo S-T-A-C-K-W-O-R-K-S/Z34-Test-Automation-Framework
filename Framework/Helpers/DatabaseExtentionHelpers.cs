7d8c2e22d437836654495b1b3384a406e42e6c50
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2019-10-23 16:20:21 +0100

    Implement Database Extension Helpers

--- Framework/Helpers/DatabaseExtentionHelpers.cs
+++ Framework/Helpers/DatabaseExtentionHelpers.cs
@@ -1,10 +1,75 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Framework.Helpers
 {
-    class DatabaseExtentionHelpers
+    public static class DatabaseExtentionHelpers
     {
+        public static SqlConnection DBConnect(this SqlConnection sqlConnection, string connectionString)
+        {
+            try
+            {
+                sqlConnection = new SqlConnection(connectionString);
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+
+            catch (Exception exception)
+            {
+                LogHelpers.WriteToLog("ERROR :: " + exception.Message);
+            }
+
+            return null;
+        }
+
+        public static void DBClose(this SqlConnection sqlConnection, string connectionString)
+        {
+            try
+            {
+                sqlConnection.Close();
+            }
+
+            catch (Exception exception)
+            {
+                LogHelpers.WriteToLog("ERROR :: " + exception.Message);
+            }
+        }
+
+        public static DataTable ExecuteQuery(this SqlConnection sqlConnection, string queryString)
+        {
+            DataSet dataSet;
+
+            try
+            {
+                if (sqlConnection == null || (sqlConnection != null && (sqlConnection.State == ConnectionState.Closed || sqlConnection.State == ConnectionState.Broken)))
+                    sqlConnection.Open();
+
+                SqlDataAdapter dataAdapter = new SqlDataAdapter();
+                dataAdapter.SelectCommand = new SqlCommand(queryString, sqlConnection);
+                dataAdapter.SelectCommand.CommandType = CommandType.Text;
+
+                dataSet = new DataSet();
+                dataAdapter.Fill(dataSet, "table");
+                sqlConnection.Close();
+
+                return dataSet.Tables["table"];
+            }
+
+            catch (Exception exception)
+            {
+                dataSet = null;
+                sqlConnection.Close();
+
+                LogHelpers.WriteToLog("ERROR :: " + exception.Message);
+                return null;
+            }
+
+            finally
+            {
+                dataSet = null;
+                sqlConnection.Close();
+            }
+        }
     }
 }
