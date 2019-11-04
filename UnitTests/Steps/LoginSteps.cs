6a5751509d898608b57c7349e82215f403204308
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2019-11-04 11:34:47 +0000

    Update Login Steps Definition

--- UnitTests/Steps/LoginSteps.cs
+++ UnitTests/Steps/LoginSteps.cs
@@ -6,40 +6,47 @@ namespace UnitTests.Steps
     [Binding]
     public sealed class LoginSteps : BasePage
     {
+        private readonly ScenarioContext _context;
+
+        public LoginSteps(ScenarioContext injectedContext)
+        {
+            _context = injectedContext;
+        }
+
         [Given]
         public void Given_I_HAVE_NAVIGATED_TO_THE_APPLICATION()
         {
-            throw new PendingStepException();
+            _context.Pending();
         }
 
         [Given]
         public void Given_I_CONFIRM_THE_APPLICATION_IS_RUNNING()
         {
-            throw new PendingStepException();
+            _context.Pending();
         }
 
         [Given]
         public void Given_I_CLICK_THE_LOGIN_LINK()
         {
-            throw new PendingStepException();
+            _context.Pending();
         }
 
         [When]
         public void When_I_ENTER_MY_USERNAME_AND_PASSWORD(Table table)
         {
-            throw new PendingStepException();
+            _context.Pending();
         }
 
         [When]
         public void When_I_CLICK_THE_LOGIN_BUTTON()
         {
-            throw new PendingStepException();
+            _context.Pending();
         }
 
         [Then]
         public void Then_I_SHOULD_BE_AUTHENTICATED_AND_SEE_A_GREETING_MESSAGE()
         {
-            throw new PendingStepException();
+            _context.Pending();
         }
     }
 }
