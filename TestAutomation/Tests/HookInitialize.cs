64b2e86c7e4d89b2d6456041e8e7bf94076775c0
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2019-11-26 22:24:45 +0000

    Tidy Up Initialization Hook & Temporarily Deprecate KLOV

--- TestAutomation/Tests/HookInitialize.cs
+++ TestAutomation/Tests/HookInitialize.cs
@@ -29,14 +29,10 @@ namespace TestAutomation.Tests
         private static ExtentTest _scenario;
 
         private static ExtentReports _extent;
-        private static ExtentKlovReporter _klov;
 
         [BeforeTestRun]
-        [SuppressMessage("Compiler Warning", "CS0162: Unreachable Code Detected", Justification = "Refactor Pending")]
-        public void BeforeTestRun()
-        {
-            InitializeConfig();
-            Settings.DatabaseConnection = Settings.DatabaseConnection.DBConnect(Settings.ConnectionString);
+        public static void BeforeTestRun()
+        { 
             string run = $"{DateTime.Now:yyyy-MM-dd_HH-mm-ss}";
 
             ExtentHtmlReporter reporter = new ExtentHtmlReporter($"..\\..\\..\\..\\REPORTS\\{run}\\");
@@ -45,24 +41,15 @@ namespace TestAutomation.Tests
             reporter.Config.Theme = Theme.Standard;
 
             _extent = new ExtentReports();
-
-            if (false) // TODO: Handle Exception Thrown While KLOV Is Not Running
-            {
-                _klov = new ExtentKlovReporter();
-                _klov.InitMongoDbConnection("localhost", 27017);
-                _klov.ProjectName = "Execute Automation Tests";
-                _klov.InitKlovServerConnection("http://localhost:5689");
-                _klov.ReportName = $"run_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}";
-
-                _extent.AttachReporter(reporter, _klov);
-            }
-
             _extent.AttachReporter(reporter);
         }
 
         [BeforeScenario]
         public void BeforeScenario()
         {
+            InitializeConfig();
+            Settings.DatabaseConnection = Settings.DatabaseConnection.DBConnect(Settings.ConnectionString);
+
             _feature = _extent.CreateTest<Feature>(_featureContext.FeatureInfo.Title);
             _scenario = _feature.CreateNode<Scenario>(_scenarioContext.ScenarioInfo.Title);
 
@@ -81,15 +68,19 @@ namespace TestAutomation.Tests
                     case "Given":
                         _scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text);
                         break;
+
                     case "When":
                         _scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text);
                         break;
+
                     case "Then":
                         _scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text);
                         break;
+
                     case "And":
                         _scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text);
                         break;
+
                     default:
                         LogHelpers.WriteToLog($"[ERROR] :: Invalid Step Type: {stepType}");
                         throw new ArgumentOutOfRangeException(nameof(stepType), stepType, $"Invalid Step Type: {stepType}");
@@ -105,15 +96,19 @@ namespace TestAutomation.Tests
                     case "Given":
                         _scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Fail(_scenarioContext.TestError.StackTrace);
                         break;
+
                     case "When":
                         _scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Fail(_scenarioContext.TestError.StackTrace);
                         break;
+
                     case "Then":
                         _scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Fail(_scenarioContext.TestError.StackTrace);
                         break;
+
                     case "And":
                         _scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text).Fail(_scenarioContext.TestError.StackTrace);
                         break;
+
                     default:
                         LogHelpers.WriteToLog($"[ERROR] :: Invalid Step Type: {stepType}");
                         throw new ArgumentOutOfRangeException(nameof(stepType), stepType, $"Invalid Step Type: {stepType}");
@@ -127,15 +122,19 @@ namespace TestAutomation.Tests
                     case "Given":
                         _scenario.CreateNode<Given>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
                         break;
+
                     case "When":
                         _scenario.CreateNode<When>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
                         break;
+
                     case "Then":
                         _scenario.CreateNode<Then>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
                         break;
+
                     case "And":
                         _scenario.CreateNode<And>(ScenarioStepContext.Current.StepInfo.Text).Skip("Step Definition Pending");
                         break;
+
                     default:
                         LogHelpers.WriteToLog($"[ERROR] :: Invalid Step Type: {stepType}");
                         throw new ArgumentOutOfRangeException(nameof(stepType), stepType, $"Invalid Step Type: {stepType}");
@@ -150,7 +149,11 @@ namespace TestAutomation.Tests
             _parallelTestExecution.Driver.Quit();
 
             LogHelpers.WriteToLog($"[END] :: {_featureContext.FeatureInfo.Title} >>> {_scenarioContext.ScenarioInfo.Title}");
+        }
 
+        [AfterTestRun]
+        public void AfterTestRun()
+        {
             _extent.Flush();
         }
     }
