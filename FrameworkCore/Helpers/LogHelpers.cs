ffac8be8136d76d10e5ca2a05b34660d9661b7a4
Vlad-Alexandru Tănăsescu <vlad-tanasescu@outlook.com>
2019-11-27 22:49:26 +0000

    Minor Changes To Log Helpers

--- FrameworkCore/Helpers/LogHelpers.cs
+++ FrameworkCore/Helpers/LogHelpers.cs
@@ -20,11 +20,11 @@ namespace FrameworkCore.Helpers
 
             StreamWriter streamWriter = File.AppendText(LogFile);
             streamWriter.WriteLine("[DEBUG] :: LOGGING STARTED" + Environment.NewLine +
-                                    "\t" + "DateTime.Now" + "\t" + "\t" + ":" + "\t" + DateTime.Now + Environment.NewLine +
-                                    "\t" + ".ToLongDateString" + "\t" + ":" + "\t" + DateTime.Now.ToLongDateString() + Environment.NewLine +
-                                    "\t" + ".ToLongTimeString" + "\t" + ":" + "\t" + DateTime.Now.ToLongTimeString() + Environment.NewLine +
-                                    "\t" + ".ToUniversalTime" + "\t" + ":" + "\t" + DateTime.Now.ToUniversalTime() + Environment.NewLine +
-                                    "\t" + ".ToLocalTime" + "\t" + "\t" + ":" + "\t" + DateTime.Now.ToLocalTime() + Environment.NewLine);
+                                   "\t" + "DateTime.Now" + "\t" + "\t" + ":" + "\t" + DateTime.Now + Environment.NewLine +
+                                   "\t" + ".ToLongDateString" + "\t" + ":" + "\t" + DateTime.Now.ToLongDateString() + Environment.NewLine +
+                                   "\t" + ".ToLongTimeString" + "\t" + ":" + "\t" + DateTime.Now.ToLongTimeString() + Environment.NewLine +
+                                   "\t" + ".ToUniversalTime" + "\t" + ":" + "\t" + DateTime.Now.ToUniversalTime() + Environment.NewLine +
+                                   "\t" + ".ToLocalTime" + "\t" + "\t" + ":" + "\t" + DateTime.Now.ToLocalTime() + Environment.NewLine);
             streamWriter.Close();
         }
 
@@ -33,7 +33,7 @@ namespace FrameworkCore.Helpers
             string logEvent = $"{DateTime.Now:dd.MM.yyyy} @ {DateTime.Now:HH.mm.ss} >>> {logMessage}" + Environment.NewLine;
             byte[] encodedText = Encoding.ASCII.GetBytes(logEvent);
 
-            using (FileStream sourceStream = new FileStream(LogFile, append ? FileMode.Append : FileMode.Create, FileAccess.Write, FileShare.None, bufferSize: 4096, useAsync: true))
+            using (FileStream sourceStream = new FileStream(LogFile, append ? FileMode.Append : FileMode.Create, FileAccess.Write, FileShare.None, 4096, true))
             {
                 await sourceStream.WriteAsync(encodedText, 0, encodedText.Length);
             }
